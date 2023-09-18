97c3608042ecc44fcc3002de0cd22c77e9f6a7d5
SarahJessiman <s223084654@mandela.ac.za>
2023-09-18 22:14:28 +0200

    Added Camera feature for profile pic to Tutor_profile and Started on Tutor_Profile_View xml

--- app/src/main/java/com/example/a2tor4you/ActivityTutorProfile.java
+++ app/src/main/java/com/example/a2tor4you/ActivityTutorProfile.java
@@ -1,14 +1,53 @@
 package com.example.a2tor4you;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ActivityTutorProfile extends AppCompatActivity {
+
+    public static final int CAMERA_ACTION_CODE = 1;
+    ImageView imageProfile;
+    Button takePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_profile);
+
+       /* imageProfile = findViewById(R.id.ImgAccount);*/ //Adds image to Account Profile pic
+        takePhoto = findViewById(R.id.btnTakePhoto);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(intent, CAMERA_ACTION_CODE   );
+                }
+                else{
+                    Toast.makeText(ActivityTutorProfile.this, "There is no app that supports this action", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null){
+            Bundle bundle = data.getExtras();
+            Bitmap finalPhoto = (Bitmap) bundle.get("data");
+            imageProfile.setImageBitmap(finalPhoto);
+        }
     }
 }
